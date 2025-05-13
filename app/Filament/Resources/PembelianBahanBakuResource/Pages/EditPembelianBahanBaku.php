c7109327a49ca61779a89efa75129ca93c131ceb
IbnuAdamAlfalah <ibnuadamalfalah@gmail.com>
2025-05-13 18:30:22 +0700

    Menyelesaikan master data vendor dan transaksi pembelian bahan baku

---
+++ app/Filament/Resources/PembelianBahanBakuResource/Pages/EditPembelianBahanBaku.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\PembelianBahanBakuResource\Pages;
+
+use App\Filament\Resources\PembelianBahanBakuResource;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPembelianBahanBaku extends EditRecord
+{
+    protected static string $resource = PembelianBahanBakuResource::class;
+}
+
